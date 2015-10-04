852febfeed072355530da8e75137bdd05805647d
Jose Molina <jmolinacolmenero@gmail.com>
2015-10-04 03:04:28 +0200

    Created main bindings

--- src/main/java/com/molina/cvmfs/client/CvmfsFileSystem.java
+++ src/main/java/com/molina/cvmfs/client/CvmfsFileSystem.java
@@ -1,34 +1,43 @@
 package com.molina.cvmfs.client;
 
+import com.molina.cvmfs.directoryentry.DirectoryEntry;
 import com.molina.cvmfs.repository.Repository;
+import com.molina.cvmfs.repository.exception.CacheDirectoryNotFound;
+import com.molina.cvmfs.repository.exception.FailedToLoadSourceException;
+import com.molina.cvmfs.rootfile.exception.RootFileException;
 import net.fusejna.*;
 import net.fusejna.util.FuseFilesystemAdapterFull;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Jose Molina Colmenero
- *
- * This interface cannot handle inodes, so some operations are not
- * identical to the original CernVM-FS. Concretely lookup and forget functions
- * are not supported
+ *         <p/>
+ *         This interface cannot handle inodes, so some operations are not
+ *         identical to the original CernVM-FS. Concretely lookup and forget functions
+ *         are not supported
  */
 public class CvmfsFileSystem extends FuseFilesystemAdapterFull {
 
     private Repository repository;
     private String cachePath;
+    private String url;
 
-    public CvmfsFileSystem(String cachePath) {
+    private Map<String, FileInputStream> openedFiles;
+
+    public CvmfsFileSystem(String url, String cachePath) {
         this.cachePath = cachePath;
+        this.url = url;
     }
 
     public CvmfsFileSystem() {
         this.cachePath = "~/.cvmfs-java-cache";
-    }
-
-
-    public FuseFilesystem log() {
-        return super.log(true);
     }
 
     private int printIlegalOperationMessage() {
@@ -38,52 +47,132 @@ public class CvmfsFileSystem extends FuseFilesystemAdapterFull {
 
     @Override
     public void init() {
-
+        openedFiles = new HashMap<String, FileInputStream>();
+        try {
+            repository = new Repository(url, cachePath);
+        } catch (FailedToLoadSourceException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CacheDirectoryNotFound cacheDirectoryNotFound) {
+            cacheDirectoryNotFound.printStackTrace();
+        } catch (RootFileException e) {
+            e.printStackTrace();
+        }
+        if (repository == null)
+            System.exit(-1);
+        log(true);
     }
 
     @Override
     public void destroy() {
-
     }
 
     @Override
     public int getattr(String path, StructStat.StatWrapper stat) {
-        return 0;
+        DirectoryEntry result = repository.lookup(path);
+        if (result != null) {
+            stat
+                    .dev(1).rdev(1)
+                    .uid(1).gid(1)
+                    .mode(result.getMode()).nlink(1)
+                    .size(result.getSize()).blksize(4096)
+                    .blocks(1 + result.getSize() / 512)
+                    .atime(result.getMtime())
+                    .mtime(result.getMtime())
+                    .ctime(result.getMtime());
+            return 0;
+        }
+        return ErrorCodes.ENOENT();
     }
 
     @Override
     public int readlink(String path, ByteBuffer buffer, long size) {
-        return 0;
+        DirectoryEntry result = repository.lookup(path, false);
+        if (result != null) {
+            if (result.isSymplink()) {
+                buffer.put(result.getSymlink().getBytes());
+                return 0;
+            } else {
+                return ErrorCodes.ENOLINK();
+            }
+        }
+        return ErrorCodes.ENOENT();
     }
 
     @Override
     public int open(String path, StructFuseFileInfo.FileInfoWrapper info) {
-        return 0;
+        File result = repository.getFile(path);
+        if (result != null) {
+            if (result.isFile()) {
+                try {
+                    FileInputStream fis = new FileInputStream(result);
+                    openedFiles.put(path, fis);
+                    info
+                            .keep_cache(false)
+                            .fh(fis.getFD().hashCode());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    ErrorCodes.ENOENT();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    ErrorCodes.ENOENT();
+                }
+                return 0;
+            } else {
+                return ErrorCodes.ENOENT();
+            }
+        }
+        return ErrorCodes.ENOENT();
     }
 
     @Override
-    public int read(String path, ByteBuffer buffer, long size, long offset, StructFuseFileInfo.FileInfoWrapper info) {
-        return 0;
+    public int read(String path, ByteBuffer buffer, long size, long offset,
+                    StructFuseFileInfo.FileInfoWrapper info) {
+        FileInputStream fis = openedFiles.get(path);
+        if (fis != null) {
+            int sizeInt = new Long(size).intValue();
+            byte[] bytes = new byte[sizeInt];
+            try {
+                int bytesRead = fis.read(bytes, ((int) offset), sizeInt);
+                if (bytesRead == sizeInt) {
+                    buffer.put(bytes);
+                    return 0;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return ErrorCodes.ENOENT();
     }
 
     @Override
     public int release(String path, StructFuseFileInfo.FileInfoWrapper info) {
-        return 0;
-    }
-
-    @Override
-    public int opendir(String path, StructFuseFileInfo.FileInfoWrapper info) {
-        return 0;
+        FileInputStream fis = openedFiles.remove(path);
+        if (fis != null) {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ErrorCodes.ENOENT();
+            }
+            return 0;
+        }
+        return ErrorCodes.ENOENT();
     }
 
     @Override
     public int readdir(String path, DirectoryFiller filler) {
-        return 0;
-    }
-
-    @Override
-    public int releasedir(String path, StructFuseFileInfo.FileInfoWrapper info) {
-        return 0;
+        DirectoryEntry result = repository.lookup(path);
+        if (result != null) {
+            if (result.isDirectory()) {
+                return 0;
+            } else {
+                return ErrorCodes.ENOTDIR();
+            }
+        } else {
+            return ErrorCodes.ENOENT();
+        }
     }
 
     @Override
@@ -93,13 +182,13 @@ public class CvmfsFileSystem extends FuseFilesystemAdapterFull {
 
     @Override
     public int getxattr(String path, String xattr, XattrFiller filler, long size, long position) {
-        // NOTE: not handle at the moment
+        // NOTE: not handled at the moment
         return super.getxattr(path, xattr, filler, size, position);
     }
 
     @Override
     public int listxattr(String path, XattrListFiller filler) {
-        // NOTE: not handle at the moment
+        // NOTE: not handled at the moment
         return super.listxattr(path, filler);
     }
 
